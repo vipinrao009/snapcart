2692a891ac6354e3e5fdfd9344c869550e71d18f
vipinrao009 <vipinrao2018@gmail.com>
2025-12-19 22:12:06 +0530

    Add Grocery model with schema definition and timestamps

---
+++ src/models/grocery.model.ts
@@ -0,0 +1,53 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IGrocery {
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+  category: string;
+  price: string;
+  unit: string;
+  image: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const grocerySchema = new Schema<IGrocery>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "Fruits & Vegitables",
+        "Dairy & Eggs",
+        "Rice, Aata & Grains",
+        "Snacks & Biscuits",
+        "Spices & Masalas",
+        "Beverages & Drinks",
+        "Personal Care",
+        "Households Essentials",
+        "Instant & Packaged Food",
+        "Baby & Pet Care",
+      ],
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    unit: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Grocery = mongoose.models.Grocery || mongoose.model("Grocery", grocerySchema);
+
+export default Grocery;
